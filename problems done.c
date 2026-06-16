81ef6cee317128a8f11e1ac3a2f73cface6c31ab
ankipop <ankipop8@gmail.com>
2026-06-17 00:15:42 +0530

    Implement area calculation for a square
    Added functionality to calculate the area of a square based on user input.

--- problems done.c
+++ problems done.c
@@ -3,6 +3,14 @@
 
 int main()
 {
-    printf("Hello world!");
+// P1.CALCULATE AREA OF A SQUARE
+
+int side;
+printf("length of side: ");
+scanf("%d", &side);
+int area=side*side;
+printf("the area is: %d", area);
+
+    
     return 0;
 }
