e2c98243b3fadf8dff72d8f18729de7c8d6a7552
ankipop <ankipop8@gmail.com>
2026-06-17 13:00:56 +0530

    Update problems done.c

--- problems done.c
+++ problems done.c
@@ -11,6 +11,14 @@ scanf("%d", &side);
 int area=side*side;
 printf("the area is: %d", area);
 
+// P2.CALCULATE AREA OF CIRCLE
+    
+float radius;
+printf("radius of the circle:  ");
+scanf("%f",&radius);
+float area=3.14*radius*radius;
+printf("the area is: %f ", area);
+    
     
     return 0;
 }
